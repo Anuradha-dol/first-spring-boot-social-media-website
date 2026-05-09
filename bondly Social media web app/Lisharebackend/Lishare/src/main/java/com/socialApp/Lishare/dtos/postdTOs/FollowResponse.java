1d5e679da326c0c2dd29465e13f0b8cdc7931c52
Anuradha-dol <anuradha123dk@gmail.com>
2026-05-09 20:01:58 +0530

    fixed erros

--- bondly Social media web app/Lisharebackend/Lishare/src/main/java/com/socialApp/Lishare/dtos/postdTOs/FollowResponse.java
+++ bondly Social media web app/Lisharebackend/Lishare/src/main/java/com/socialApp/Lishare/dtos/postdTOs/FollowResponse.java
@@ -1,10 +1,14 @@
 package com.socialApp.Lishare.dtos.postdTOs;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FollowResponse {
     private Long userId;
     private String firstName;
